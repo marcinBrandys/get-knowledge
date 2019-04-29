f303311efd5aeb59b1d9fa4965cdf2ee6508aaab
Marcin Brandys <marcin.brandys.ext@gigaset.com>
2019-04-29 14:10:12 +0200

    rest_https

--- get-knowledge-rest/src/server.ts
+++ get-knowledge-rest/src/server.ts
@@ -3,20 +3,26 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const appConfig = require('./config/config');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
+const certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 const userManagementRoutes = require('./routes/api-user-management-routes');
 const groupManagementRoutes = require('./routes/api-group-management-routes');
 const taskGroupManagementRoutes = require('./routes/api-task-group-management-routes');
 const taskManagementRoutes = require('./routes/api-task-management-routes');
 const solutionManagementRoutes = require('./routes/api-solution-management-routes');
 
-mongoose.connect(appConfig.DB_PATH, {
-        useNewUrlParser: true, useCreateIndex: true
-    }).then(function () {
-        console.log('successfully connected with mongodb');
-    }).catch(function (error) {
-        console.log('mongodb connection error:', error);
-    });
-const db = mongoose.connection;
+// mongoose.connect(appConfig.DB_PATH, {
+//         useNewUrlParser: true, useCreateIndex: true
+//     }).then(function () {
+//         console.log('successfully connected with mongodb');
+//     }).catch(function (error) {
+//         console.log('mongodb connection error:', error);
+//     });
+// const db = mongoose.connection;
 
 const app = express();
 
@@ -32,6 +38,14 @@ app.use('/api/task-group-management', taskGroupManagementRoutes);
 app.use('/api/task-management', taskManagementRoutes);
 app.use('/api/solution-management', solutionManagementRoutes);
 
-app.listen(appConfig.PORT, function () {
-    console.log('app is working')
+app.get('/status', function(req,res) {
+    res.send('hello');
 });
+
+// app.listen(appConfig.PORT, function () {
+//     console.log('app is working')
+// });
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+httpServer.listen(8080);
+httpsServer.listen(8443);
