90abec2efa743302234c81366b6d597f654ba97b
Marcin Brandys <marcinbrandys95@gmail.com>
2019-05-01 18:39:03 +0200

    Rest api prod config

--- get-knowledge-frontend/src/app/services/rest.service.ts
+++ get-knowledge-frontend/src/app/services/rest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {environment} from "../../environments/environment.prod";
+import {environment} from "../../environments/environment";
 import {AuthService} from "./auth.service";
 import * as _ from 'lodash';
 import {Solution} from "../classes/solution";
